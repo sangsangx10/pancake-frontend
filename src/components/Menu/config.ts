03e6f8ec44c1bbec0de36085e6c81c24803a0068
Ichika Nakano <ichikanakano@protonmail.com>
2021-01-28 19:48:10 +0900

    up

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,7 +12,7 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.pancakeswap.finance',
+        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xc808592e5b4585f982bc728d63b227634bc007a4',
       },
       {
         label: 'Liquidity',
